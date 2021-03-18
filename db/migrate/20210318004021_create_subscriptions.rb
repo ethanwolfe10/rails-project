1576d17e20a292d754754980f5165ed820d9fcf8
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-17 17:42:29 -0700

    added subscription model that joins a user and a group, determines if user is a moderator and that a confirmation that they belong to the group

---
+++ db/migrate/20210318004021_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :subscriptions do |t|
+      t.integer :user_id
+      t.integer :group_id
+      t.boolean :moderator
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+  end
+end
