23e95874b8be4f2c98059d70c42e5ce13d4b521b
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-17 18:04:31 -0700

    started forming relationships with other models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :subscriptions
+    has_many :groups, through: :subscriptions
+    has_many :followers
+    has_many :followings
+    has_many :posts, through: :groups
+    has_many :likes
+    has_many :comments
 end
