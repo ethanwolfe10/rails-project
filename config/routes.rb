c1c2a80ca42741600a3044cb6e5770a628a1cac5
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-18 19:08:10 -0700

    added create route to post nested under groups. and created group routes new edit and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups, only: [:show] do 
-    resources :posts, only: [:new, :show]
+    resources :posts, only: [:new, :show, :create]
   end
+
+  resources :groups, only: [:new, :edit, :create]
 
   resources :users, only: [:show] do
     get '/followers' => 'followers#show'
