60a5729c25ad4dc6a8ccd00274b0674695d08ac2
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-19 19:19:31 -0700

    added omniauth and devise gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     has_many :subscriptions
     has_many :groups, through: :subscriptions
     has_many :followers
