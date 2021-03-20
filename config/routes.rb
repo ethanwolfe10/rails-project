42c6d85898a4152a060c42181adf888bbd826151
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-19 19:45:21 -0700

    changed devise routing for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
   resources :groups, only: [:show] do 
     resources :posts, only: [:new, :show, :create, :edit]
   end
