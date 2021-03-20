8e0461f21f2d3009fd10c93c59f20177b3c3bb77
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-19 18:31:38 -0700

    removed post_id column from groups because a group has many posts

--- db/migrate/20210318003821_create_groups.rb
+++ db/migrate/20210318003821_create_groups.rb
@@ -2,8 +2,7 @@ class CreateGroups < ActiveRecord::Migration[6.1]
   def change
     create_table :groups do |t|
       t.string :name
-      t.integer :post_id
-
+      
       t.timestamps
     end
   end
