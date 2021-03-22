7647ee06e7290e5f26681f7920c033588008739f
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-21 21:37:25 -0700

    added new and create action for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,12 @@
 class GroupsController < ApplicationController
+
+    def new
+        if !user_signed_in?
+            redirect_to '/users/sign_in'
+        end
+    end
+
+    def create
+        binding.pry
+    end
 end
