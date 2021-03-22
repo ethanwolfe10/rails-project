d96a0c1726906505a7c4d5f74220e7d211956bc9
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-21 20:03:55 -0700

    finished devise routes with omniauth_callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  root: 'static#home'
+  root 'static#home'
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
   resources :groups, only: [:show] do 
     resources :posts, only: [:new, :show, :create, :edit]
   end
