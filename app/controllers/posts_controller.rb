31c2cc16fc2690eb0c2d3e3138abd1445d8e67fd
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-23 18:20:56 -0700

    add new, create and show routes. added strong params for post creation."

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
+    def new
+        @post = Post.new(user_id: current_user.id, group_id: params[:group_id])
+    end
+
+    def create
+        @post = Post.create(post_params)
+        if @post.save
+            redirect_to group_path(@post.group_id)
+        else
+            redirect_to new_group_post_path
+        end
+    end
+
+    def show
+        @post = Post.find_by(id: params[:id])
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:content, :group_id, :user_id)
+    end
 end
