bd42ffb7f5aad8a3204e03d4767393b99d682fd9
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-27 19:54:21 -0700

    a user can now edit their displayname and their bio. bio can be blank but a displayname cannot.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
+    def index
+    end
 
     def show
         if params[:user_id] && !User.exists?(params[:user_id])
             redirect_to new_user_session_path
         else
-           @current_user = User.find(params[:id])
+           @user = User.find(params[:id])
         end
+    end
+
+    def edit
+        if current_user.id != params[:id].to_i
+            redirect_to user_path(current_user)
+        end
+    end
+
+    def update
+        if !params[:user][:display_name].blank?
+            current_user.update(display_name: params[:user][:display_name], bio: params[:user][:bio])
+            redirect_to user_path(current_user)   
+        else
+            redirect_to edit_user_path(current_user)
+            flash[:alert] = "Invalid Display Name"
+        end            
     end
     
 end
