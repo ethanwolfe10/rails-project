7f55e89ce3f438e9278f37c032924dfefaa7ac3d
ethanwolfe10 <ethanwolfe@comcast.net>
2021-03-29 18:19:24 -0700

    upon user creation from omniauth a displayname would not be created so now it defaults to the username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
     has_many :subscriptions
     has_many :groups, through: :subscriptions
-    has_many :followers
+    has_many :follows
     has_many :posts, through: :groups
     has_many :likes
     has_many :comments
@@ -16,7 +16,7 @@ class User < ApplicationRecord
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
         user.display_name = auth.info.name   # assuming the user model has a name
-        binding.pry
+        user.username = auth.info.name   # assuming the user model has a name
         # user.image = auth.info.image # assuming the user model has an image
         # If you are using confirmable and the provider(s) you use validate emails, 
         # uncomment the line below to skip the confirmation emails.
